016f919b3e0fb8c4f5f303ca33b7e7ef3941be81
m-n-sakib-dev <mdsakib.ndc17@gmail.com>
2026-04-19 02:30:56 +0600

    api updated.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,14 +1,13 @@
 const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
+  }
 
-//   if (typeof window !== "undefined") {
-//     return ""; 
-//   }
-  
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
-  
-  return "http://localhost:3001"; 
+
+  return "http://localhost:3001";
 };
 
 export const API_BASE_URL = getBaseUrl();
