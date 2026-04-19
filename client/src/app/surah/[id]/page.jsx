fcab2feadcdb091ae845feeac64945c82baa8548
Nazmul Sakib <mdsakib.ndc17@gmail.com>
2026-04-19 10:24:51 +0600

    .map is not a function is updated

--- client/src/app/surah/[id]/page.jsx
+++ client/src/app/surah/[id]/page.jsx
@@ -21,16 +21,17 @@ export default async function SurahDetailPage({ params }) {
         <div className="text-center text-[34px]">{surah.info.name_ar}</div>
         <div className="text-center text-[24px]">{surah.info.name_en}</div>
       </div>
-      {surah.ayahs.map((ayah) => (
+      {(surah.ayahs || []).map((ayah) => (
         <div className="p-4 border-t" key={ayah.number_in_surah}>
           <p className="text-right">
             <SettingsWrapper type="arabic">{ayah.text}</SettingsWrapper>
           </p>
-          <p className=" mt-2">
+          <p className="mt-2">
             <SettingsWrapper type="translation">{ayah.data}</SettingsWrapper>
           </p>
         </div>
       ))}
+
     </div>
   );
 }
