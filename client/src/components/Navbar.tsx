2358c0bbf74e6b6c027359ea2daec1455e3a6df1
m-n-sakib-dev <mdsakib.ndc17@gmail.com>
2026-05-08 14:30:12 +0600

    navbar left incon added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 
 export default function Navbar() {
     const [isRightSidebarOpen, setRightSidebarOpen] = useState<boolean>(false);
+    const [isLeftSidebarOpen, setLeftSidebarOpen] = useState<boolean>(false);
     const [isLargeScreen, setIsLargeScreen] = useState<boolean>(false);
     useEffect(() => {
         setIsLargeScreen(window.innerWidth >= 1024);
@@ -15,6 +16,15 @@ export default function Navbar() {
         <>
             <nav className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-md">
                 <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between">
+                    <div className="lg:hidden">
+                        <button
+                            onClick={() => setLeftSidebarOpen(true)}
+                            className="bg-gray-100 hover:bg-gray-200 p-2 rounded-lg transition-all cursor-pointer text-xl"
+                            title="Menu"
+                        >
+                            ☰
+                        </button>
+                    </div>
 
                     <Link href="/" className="font-bold text-lg md:text-xl flex-shrink-0">
                         Web Quran
@@ -34,10 +44,20 @@ export default function Navbar() {
                             ⚙️
                         </button>
                     </div>
+
                 </div>
             </nav>
 
-            {/* Settings Sidebar */}
+            {!isLargeScreen && (<div
+                className={`fixed inset-y-0 left-0 w-72 shadow-2xl transform ${isLeftSidebarOpen ? "translate-x-0" : "-translate-x-full"
+                    } transition-transform duration-300 ease-in-out p-6 z-50 bg-gray-300`}
+            >
+
+                <SettingsSidebar
+                    onClose={() => setLeftSidebarOpen(false)}
+                />
+            </div>)}
+
             {!isLargeScreen && (<div
                 className={`fixed inset-y-0 right-0 w-72 shadow-2xl transform ${isRightSidebarOpen ? "translate-x-0" : "translate-x-full"
                     } transition-transform duration-300 ease-in-out p-6 z-50 bg-gray-300`}
